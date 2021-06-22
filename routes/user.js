4f6c9884b8019a9b8b5de28ba322120d8120bb04
Sunwit Petchoo <sunwit.p@gmail.com>
2021-06-22 21:07:57 +1000

    Add user function, add 404 page and create schedule view

---
+++ routes/user.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../database')
+const { redirectToLogin } = require('../middlewares/auth')
+
+router.get('/:id(\\d+)', redirectToLogin, (req, res) => {
+    const query = "SELECT u.user_id as user_id, day, start_at, end_at, sur_name, first_name, email " + 
+    "FROM schedules s INNER JOIN users u ON s.user_id = u.user_id where u.user_id = $1;"
+     db.any(query, req.params.id)
+     .then((schedules) =>{
+        if(schedules.length > 0){
+            res.render('pages/user',{
+                schedules: schedules,
+                layout:'./layouts/full-width' 
+                
+            })
+        }else{
+            res.redirect('/404')
+        }
+       
+     })
+     .catch((err) => {
+        res.send(err.message)
+    })
+    })
+
+module.exports = router
